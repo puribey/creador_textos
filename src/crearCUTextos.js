d84c99698eb64723d82987a29336ff1924467827
Priscila Bey <pri.bey@gmail.com>
2021-05-31 18:48:38 -0300

    entrega_2_crear_texto

--- src/crearCUTextos.js
+++ src/crearCUTextos.js
@@ -1,18 +1,20 @@
 import { crearErrorIdTextoRepetido } from "./errores/ErrorIdTextoRepetido.js";
-
 import { crearTexto } from "./texto.js";
 
-function crearApiTextos({ daoTextos }) {
+function crearCUTextos({ daoTextos }) {
   return {
     addNew: async (datosTexto) => {
       const texto = crearTexto(datosTexto);
-      const { added } = await daoTextos.addUnique(texto, "idTexto");
+      const { added } = await daoTextos.addNew(texto, "idTexto");
       if (!added) {
         throw crearErrorIdTextoRepetido("ya existe un texto con este id");
       }
       return texto;
     },
+    getAllByUser: async (idUsuario) => {
+      return await daoTextos.getAllByUser(idUsuario);
+    },
   };
 }
 
-export { crearApiTextos };
+export { crearCUTextos };
